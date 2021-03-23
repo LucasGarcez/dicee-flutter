785111e861d0a4d320353b7d576af301c27fc190
LucasGarcez <lucasgarcezdev@gmail.com>
2021-03-22 22:15:52 -0300

    changeDiceFace method

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,6 +26,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 2;
   int rightDiceNumber = 3;
 
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,9 +44,7 @@ class _DicePageState extends State<DicePage> {
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
                   onPressed: () {
-                    setState(() {
-                      leftDiceNumber = 5;
-                    });
+                    changeDiceFace();
                   },
                   child: Image.asset('images/dice$leftDiceNumber.png')),
             ),
@@ -48,7 +55,7 @@ class _DicePageState extends State<DicePage> {
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
                   onPressed: () {
-                    print('Right was pressed');
+                    changeDiceFace();
                   },
                   child: Image.asset('images/dice$rightDiceNumber.png')),
             ),
